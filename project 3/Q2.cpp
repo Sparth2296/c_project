21c89a1e73ec46b1571e00e7a571cbf913d60313
sparth2296 <solankiparth94274@gmail.com>
2024-06-26 08:52:45 +0530

    project 3

---
+++ project 3/Q2.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main(){
+	 
+	 int num,total;
+	 
+	 printf("Enter the any number :");
+	 scanf("%d",&num);// user input number.
+	 
+	 if (num<0){
+	 	num =-num;// negetive number
+	 }
+	 
+	 if(num==0){
+	 	total =1;//positive number
+	 }else{
+	 	
+	 	while(num!=0){
+	 		num/=100;
+	 		total++; //increment 
+		 }
+	 }
+	 
+	 
+	 
+	 
+	 printf("Total number of digits :%d\n",total);
+     
+}
